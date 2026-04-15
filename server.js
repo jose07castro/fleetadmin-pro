312f70f23a03843bcb7e8d70d59d4890ab4ea6b8
Jose <jose@fleetadmin.com>
2026-04-15 05:58:13 -0300

    1865784521

--- server.js
+++ server.js
@@ -3,20 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// EL "HACK" DEFINITIVO: Obliga a que todo se envíe como UTF-8
-app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    next();
-});
+// Servir archivos de la carpeta raíz
+app.use(express.static(__dirname));
 
-// Servir archivos estáticos (CSS, JS, Imágenes)
-app.use(express.static(path.join(__dirname)));
-
-// Ruta principal para el index.html
+// Ruta principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor FleetAdmin Pro corriendo en puerto ${PORT}`);
+    console.log('Servidor activo en puerto ' + PORT);
 });
