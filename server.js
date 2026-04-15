7546c3cf9b605966739feed66fa6af03c5dcf16c
Jose <jose@fleetadmin.com>
2026-04-15 06:27:37 -0300

    server0000254

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-// Render usa el puerto que él quiere, por eso usamos process.env.PORT
 const PORT = process.env.PORT || 10000;
 
-// Servir archivos estáticos desde la raíz
-app.use(express.static(__dirname));
-
-// Ruta principal
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+// 1. Forzar que todo sea UTF-8 para matar los jeroglíficos
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    next();
 });
 
-// Arrancar el motor
+// 2. Servir archivos desde la raíz Y desde la carpeta www
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'www')));
+
+// 3. Ruta principal: busca el index.html en todos lados
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'), (err) => {
+        if (err) {
+            res.sendFile(path.join(__dirname, 'www', 'index.html'), (err2) => {
+                if (err2) {
+                    res.status(404).send('No se encontró el index.html. Verificá las carpetas.');
+                }
+            });
+        }
+    });
+});
+
 app.listen(PORT, () => {
-    console.log('Servidor FleetAdmin Pro en marcha en puerto ' + PORT);
+    console.log('Servidor FleetAdmin Pro rugiendo en el puerto ' + PORT);
 });
