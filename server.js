45562ecfb0f96d030f06a3596b1fd08dc4f8b980
Jose <jose@fleetadmin.com>
2026-04-15 06:00:29 -0300

    12345695487

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// Render usa el puerto que él quiere, por eso usamos process.env.PORT
 const PORT = process.env.PORT || 10000;
 
-// Servir archivos de la carpeta raíz
+// Servir archivos estáticos desde la raíz
 app.use(express.static(__dirname));
 
 // Ruta principal
@@ -11,6 +13,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Arrancar el motor
 app.listen(PORT, () => {
-    console.log('Servidor activo en puerto ' + PORT);
+    console.log('Servidor FleetAdmin Pro en marcha en puerto ' + PORT);
 });
