05814fcf608d40756df519feb63b76422d9d8817
Jose <jose@fleetadmin.com>
2026-04-15 08:04:50 -0300

    sever 653421

--- server.js
+++ server.js
@@ -3,29 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// 1. Forzar que todo sea UTF-8 para matar los jeroglíficos
-app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    next();
-});
-
-// 2. Servir archivos desde la raíz Y desde la carpeta www
+// 1. Dejamos que Express sirva los archivos libremente (JS, CSS, HTML, lo que sea)
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'www')));
 
-// 3. Ruta principal: busca el index.html en todos lados
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'), (err) => {
-        if (err) {
-            res.sendFile(path.join(__dirname, 'www', 'index.html'), (err2) => {
-                if (err2) {
-                    res.status(404).send('No se encontró el index.html. Verificá las carpetas.');
-                }
-            });
-        }
-    });
+// 2. Ruta principal: pase lo que pase, te lleva al inicio de la app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// 3. Arranque del motor
 app.listen(PORT, () => {
     console.log('Servidor FleetAdmin Pro rugiendo en el puerto ' + PORT);
 });
